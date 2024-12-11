6e44b2afee0089b215aa5ee2ca325948333de6a0
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-12-11 05:20:23 +0200

    init+auth

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { env } from "./utils/env.js";
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { logger } from './middlewares/logger.js';
+
+//import contactsRouter from "./routers/contacts.js";
+import authRouter from "./routers/auth.js";
+import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
+
+export const setupServer = () => {
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(express.static("uploads"));
+    //app.use(logger);
+
+    app.use('/auth', authRouter);
+    //app.use('/contacts', contactsRouter);
+    //app.use('/uploads', express.static(UPLOAD_DIR));
+    //app.use('/api-docs', swaggerDocs());	
+    app.use(notFoundHandler);
+
+    app.use(errorHandler);
+
+	const port = Number(env("PORT", 3000));
+	app.listen(port, () => console.log(`Server is running on ${port}`));
+};
---
+++ src/validation/auth.js
@@ -0,0 +1,29 @@
+import Joi from "joi";
+
+import { emailRegexp } from "../constants/users.js";
+
+// signup
+export const authRegisterSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().pattern(emailRegexp).required(),
+    password: Joi.string().min(6).required(),
+});
+
+// signin
+export const authLoginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+});
+
+export const requestResetEmailSchema = Joi.object({
+    email: Joi.string().email().required(),
+});
+
+export const resetPasswordSchema = Joi.object({
+    password: Joi.string().required(),
+    token: Joi.string().required(),
+});
+// OAuth
+export const loginWithGoogleOAuthSchema = Joi.object({
+    code: Joi.string().required(),
+});
