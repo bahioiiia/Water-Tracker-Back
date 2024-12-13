4713e753c94de4b02bffb98a5fe0e3e32653c99a
Ivan <65630800+bahioiiia@users.noreply.github.com>
2024-12-13 19:19:41 +0200

    Update water.js

--- src/routers/water.js
+++ src/routers/water.js
@@ -1,39 +1,23 @@
-import { Router } from 'express';
+import { Router } from "express";
 import * as waterController from '../controllers/water.js';
 
-import ctrlWrapper from '../utils/ctrlWrapper.js';
-import validateBody from '../utils/validateBody.js';
+import ctrlWrapper from "../utils/ctrlWrapper.js";
+import validateBody from "../utils/validateBody.js";
 
-import { glassAddSchema, glassUpdateSchema } from '../validation/water.js';
+import { glassAddSchema, glassUpdateSchema } from "../validation/water.js";
 import { isValidGlassId } from '../middlewares/isValidId.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import { authenticate } from "../middlewares/authenticate.js";
 
 const waterRouter = Router();
 
 waterRouter.use(authenticate);
 
-waterRouter.post(
-  '/glass',
-  validateBody(glassAddSchema),
-  ctrlWrapper(waterController.addGlassController),
-);
+waterRouter.post('/glass', validateBody(glassAddSchema), ctrlWrapper(waterController.addGlassController));
 
-waterRouter.patch(
-  '/glass/:glassId',
-  isValidGlassId,
-  validateBody(glassUpdateSchema),
-  ctrlWrapper(waterController.patchGlassController),
-);
-waterRouter.delete(
-  '/glass/:glassId',
-  isValidGlassId,
-  ctrlWrapper(waterController.deleteGlassController),
-);
+waterRouter.patch('/glass/:glassId', isValidGlassId, validateBody(glassUpdateSchema), ctrlWrapper(waterController.patchGlassController));
+waterRouter.delete('/glass/:glassId', isValidGlassId, ctrlWrapper(waterController.deleteGlassController));
 
-waterRouter.get(
-  '/daily/:userId',
-  ctrlWrapper(waterController.getDailyController),
-);
+waterRouter.get('/daily/:userId', ctrlWrapper(waterController.getDailyController));
 waterRouter.get('/monthly', ctrlWrapper(waterController.getMonthlyController));
 
 export default waterRouter;
