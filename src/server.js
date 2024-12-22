d313e1e70e151b165e24bb7058408ca34f586aab
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-12-22 18:28:31 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ import authRouter from "./routers/auth.js";
 import { UPLOAD_DIR } from './constants/index.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
-const allowedOrigins = ['http://localhost:5173', 'https://water-tracker-murex.vercel.app/'];
+const allowedOrigins = ['https://water-tracker-murex.vercel.app', 'http://localhost:5173'];
 
 const corsOptions = {
     origin: function (origin, callback) {
@@ -29,8 +29,8 @@ const corsOptions = {
 export const setupServer = () => {
     const app = express();
 
-    //app.use(cors(corsOptions));
-    app.use(cors({ origin: 'https://water-tracker-murex.vercel.app', credentials: true }));
+    app.use(cors(corsOptions));
+//    app.use(cors({ origin: 'https://water-tracker-murex.vercel.app', credentials: true }));
     app.use(express.json());
     app.use(cookieParser());
     app.use(express.static('uploads'));
